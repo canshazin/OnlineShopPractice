f256e43350cc4627163ce79405f69c982752ead5
canshazin <shazinshazz@gmail.com>
2024-05-01 13:37:04 +0530

    delete button functionality

--- models/product.js
+++ models/product.js
@@ -1,7 +1,11 @@
 const fs = require("fs");
 const path = require("path");
 
-const p = path.join(__dirname, "..", "data", "products.json");
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "products.json"
+);
 
 const getProductsFromFile = (cb) => {
   fs.readFile(p, (err, fileContent) => {
@@ -14,7 +18,8 @@ const getProductsFromFile = (cb) => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -22,22 +27,45 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString();
     getProductsFromFile((products) => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.log(err);
-      });
+      if (this.id) {
+        const existingProductIndex = products.findIndex(
+          (prod) => prod.id === this.id
+        );
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+          console.log(err);
+        });
+      } else {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), (err) => {
+          console.log(err);
+        });
+      }
     });
   }
 
   static fetchAll(cb) {
     getProductsFromFile(cb);
   }
+
   static findById(id, cb) {
     getProductsFromFile((products) => {
-      const product = products.find((p) => p.id == id);
+      const product = products.find((p) => p.id === id);
       cb(product);
+    });
+  }
+  static deleteproductbyID(id) {
+    getProductsFromFile((products) => {
+      // console.log(products);
+      const existingProductIndex = products.findIndex((prod) => prod.id === id);
+      products.splice(existingProductIndex, 1);
+      // console.log(existingProductIndex);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
     });
   }
 };
